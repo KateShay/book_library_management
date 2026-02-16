11435e758b4fb7fb899c955640d7dca24bbd7c9e
KateShay <ximoeko2017@gmail.com>
2026-02-17 01:12:58 +0700

    creation models

--- manage_book/web_library/models.py
+++ manage_book/web_library/models.py
@@ -4,7 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
-    isbn = models.IntegerField()
+    isbn = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
