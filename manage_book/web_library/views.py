7ebaaaa8c9d5f91ffb44ed47078053e10467ba0d
KateShay <ximoeko2017@gmail.com>
2026-02-17 01:21:09 +0700

    creation search

--- manage_book/web_library/views.py
+++ manage_book/web_library/views.py
@@ -1,7 +1,18 @@
+import Q
 from django.shortcuts import render
 from .models import Book
-
+from django.db.models import Q
 
 def index (request):
     books = Book.objects.all()
-    return render(request, 'index.html', {'books': books})
+
+    search_books = request.GET.get('search', '')
+    if search_books:
+        books = Book.objects.filter(
+            Q(title__icontains=search_books) |
+            Q(author__icontains=search_books)
+        )
+
+    return render(request, 'index.html', {
+        'books': books,
+        'search_books': search_books})
